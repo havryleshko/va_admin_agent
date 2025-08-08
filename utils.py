c5cdf9bdee48b60452154932b0ff59317df5c3f8
Alex Havryleshko <alexhavryleshko@gmail.com>
2025-08-08 14:16:31 +0100

    bug fix in utils

--- utils.py
+++ utils.py
@@ -14,6 +14,7 @@ def get_redirect():
 def get_gmail():
     if "credentials" in st.session_state:
         return build('gmail', 'v1', credentials=st.session_state["credentials"])
+    
 
     try:
         web_config = dict(st.secrets["gmail_credentials"]["web"])
