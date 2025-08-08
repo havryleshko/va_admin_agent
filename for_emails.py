ccb73b2476351f48a8d0ac258c5644fad5d13aa3
Alex Havryleshko <alexhavryleshko@gmail.com>
2025-08-08 15:52:02 +0100

    bug fix

--- for_emails.py
+++ for_emails.py
@@ -2,30 +2,43 @@ from utils import get_gmail
 from llm_central import llm_clf
 from llm_central import llm_draft_reply
 from utils import send_email
+from googleapiclient.errors import HttpError
 
 
 def get_unread_emails():
     service = get_gmail()
-    results = service.users().messages().list(userId='me', q='is:unread').execute()
+    if service is None:
+        raise RuntimeError("Please sign in first")
+    
+    try:
+        results = service.users().messages().list(userId='me', q='is:unread').execute()
+    except HttpError as e:
+        print(f"Debug: Gmail API {e}")
+        return []
     messages = results.get('messages', []) # returning messages
     emails = [] # creates actual full list for storing all data later
 
 
     for m in messages: #for each message get its details
-        m_id = m['id'] #need the email id to get full content
-        m_data = service.users().messages().get(userId='me', id=m_id, format='full').execute() #pulling all content 
+        try:
+            m_id = m['id'] #need the email id to get full content
+            m_data = service.users().messages().get(userId='me', id=m_id, format='full').execute() #pulling all content 
 
-        headers = m_data['payload'].get('headers', []) #getting header from email
-        subject = next((header['value'] for header in headers if header['name'] == 'Subject'), 'No Subject') # getting subject of email
-        sender = next((header['value'] for header in headers if header['name'] == 'From'), 'Unknown Sender') #getting info about sender
-        snippet = m_data.get('snippet', '') #getting first few lines from the message to display
+            headers = m_data['payload'].get('headers', []) #getting header from email
+            subject = next((header['value'] for header in headers if header['name'] == 'Subject'), 'No Subject') # getting subject of email
+            sender = next((header['value'] for header in headers if header['name'] == 'From'), 'Unknown Sender') #getting info about sender
+            snippet = m_data.get('snippet', '') #getting first few lines from the message to display
 
-        emails.append({
-            'id': m_id,
-            'subject': subject,
-            'sender': sender,
-            'snippet': snippet
-        }) # creating dict into one obj
+            emails.append({
+                'id': m_id,
+                'subject': subject,
+                'sender': sender,
+                'snippet': snippet
+            }) # creating dict into one obj
+
+        except HttpError as e:
+            print(f'Debug: error retrieving message {m_id}, {e}')
+            continue
 
     return emails
 
